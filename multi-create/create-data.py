9430e284d22a0b9f01b31bfcc2941a5791bedd81
rumesh <rumesh.m.s16@gmail.com>
2022-02-01 10:53:58 +0530

    create multitask

---
+++ multi-create/create-data.py
@@ -0,0 +1,53 @@
+import json
+from typing import List, Dict
+
+
+# mined ==> po,pt
+# newsela si ==> com,sim
+# newsela ==> come,sime
+
+
+def get_data_from_dataset(data_file_name: str, key1="com", key2="sim"):
+    key1_list = []
+    key2_list = []
+    dataset = json.load(open(data_file_name))['data']
+    for ins in dataset:
+        key1_list.append(ins[key1])
+        key2_list.append(ins[key2])
+    assert len(key1_list) == len(key2_list)
+    return key1_list, key2_list
+
+
+def create_new_dataset(key1_list: List[str], key2_list: List[str], new_key1="lang1", new_key2="lang2"):
+    new_dataset = dict()
+    new_dataset["data"] = []
+    assert len(key1_list) == len(key2_list)
+    for i, j in zip(key1_list, key2_list):
+        new_dataset["data"].append({new_key1: i, new_key2: j})
+    return new_dataset
+
+
+def write_dataset_to_file(dataset: Dict, file_name="new_dataset.json"):
+    file = open(file_name, "w")
+    json.dump(dataset, file, ensure_ascii=False, indent=4)
+
+
+if __name__ == '__main__':
+    file_names = ["newsela-si-valid-tt.json"]
+    keys = ["com,sim"]
+
+    new_data_key1 = []
+    new_data_key2 = []
+    for name, keys in zip(file_names, keys):
+        k1 = keys.split(",")[0].strip()
+        k2 = keys.split(",")[1].strip()
+        d1, d2 = get_data_from_dataset(name, k1, k2)
+        # Do subsampling here if required
+
+        new_data_key1.extend(d1)
+        new_data_key2.extend(d2)
+    print(len(new_data_key1))
+
+    # Add task token here if required
+    data_dict = create_new_dataset(new_data_key1, new_data_key2, new_key1="lang1", new_key2="lang2")
+    write_dataset_to_file(data_dict, "multitask-data.json")
