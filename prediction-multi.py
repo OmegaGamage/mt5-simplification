819f76483daa8fa3d7cdc922e0036e3ffedbd12f
rumesh <rumesh.m.s16@gmail.com>
2022-03-01 23:29:38 +0530

    added forced bos token to gen

--- prediction-multi.py
+++ prediction-multi.py
@@ -1,6 +1,5 @@
-from transformers import MBartModel, MBartTokenizer
+import transformers
 import argparse
-import json
 import torch
 import logging
 
@@ -21,6 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--rep_pen', type=float, help=f'Repetition penalty.')
     parser.add_argument('--num_beams', type=int, help="Number of beams. Default is 1")
     parser.add_argument('--do_sample', type=bool, help="Whether to use sampling. Default False")
+    parser.add_argument('--forced_bos_token_id', type=str, help="Forced BOS token to select task")
 
     args = parser.parse_args()
 
@@ -37,13 +37,18 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Load model and tokenizer from path
-    model = MBartModel.from_pretrained(args.model_path)
+    model = transformers.AutoModelForSeq2SeqLM.from_pretrained(args.model_path)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_path)
+    forced_bos_token_id = (
+        tokenizer.lang_code_to_id[args.forced_bos_token] if args.forced_bos_token is not None else None
+    )
+    model.config.forced_bos_token_id = forced_bos_token_id
     model.to(device)
-    tokenizer = MBartTokenizer.from_pretrained(args.model_path)
+
     logging.info("Model and Tokenizer loaded successfully. Starting predictions.")
 
     # Set max length
-    max_length = args.max_length if (args.max_length) else 700
+    max_length = args.max_length if args.max_length else 700
     logging.info("Max length set to %d." % max_length)
 
     # Set verbosity.
@@ -56,18 +61,18 @@ if __name__ == '__main__':
     logging.info("Verbosity set to %s." % args.verbosity)
 
     # Set count
-    count = args.count if (args.count) else len(source_sentences)
+    count = args.count if args.count else len(source_sentences)
 
-    temp = args.temp if (args.temp) else 1.0
+    temp = args.temp if args.temp else 1.0
     logging.info("Temperature set to %f." % temp)
 
-    topk = args.topk if (args.topk) else 50
+    topk = args.topk if args.topk else 50
     logging.info("Top k set to %d." % topk)
 
-    topp = args.topp if (args.topp) else 1.0
+    topp = args.topp if args.topp else 1.0
     logging.info("Top p set to %f." % topp)
 
-    rep_pen = args.rep_pen if (args.rep_pen) else 1.0
+    rep_pen = args.rep_pen if args.rep_pen else 1.0
     logging.info("Repeptition penalty set to %f." % rep_pen)
 
     num_beams = args.num_beams if args.num_beams else 1
@@ -84,7 +89,7 @@ if __name__ == '__main__':
         input_ids = tokenizer(line, return_tensors="pt").input_ids
         input_ids = input_ids.to(device)
         output_ids = model.generate(input_ids=input_ids, do_sample=do_sample, temperature=temp, max_length=max_length,
-                                    top_k=topk, top_p=topp, repetition_penalty=rep_pen,num_beams=num_beams)
+                                    top_k=topk, top_p=topp, repetition_penalty=rep_pen, num_beams=num_beams)
         out = tokenizer.decode(output_ids[0])
 
         # Remove pad and eos tokens.
@@ -94,4 +99,4 @@ if __name__ == '__main__':
         out = out.replace("\u0dca \u0dbb", "\u0dca\u200d\u0dbb").replace("\u0dca \u0dba", "\u0dca\u200d\u0dba")
         outfile.write('%s \n' % out)
         i += 1
-        if (i % 500 == 0): logging.info("%s sentences completed." % i)
+        if i % 500 == 0: logging.info("%s sentences completed." % i)
